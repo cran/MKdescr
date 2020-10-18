bc9dc4561d1aaf2f05aa74fc9d66fb43ad4ba560
Matthias Kohl <Matthias.Kohl@stamats.de>
2020-10-18 20:50:08 +0000

    version 0.6

--- R/CV.R
+++ R/CV.R
@@ -1,20 +1,23 @@
 CV <- function(x, na.rm = FALSE){
   stopifnot(is.numeric(x))
+  if(na.rm) x <- x[!is.na(x)]
   if(any(x <= 0))
     stop("Your data must be positive!")
-  sd(x, na.rm = na.rm)/mean(x, na.rm = na.rm)
+  sd(x)/mean(x)
 }
 
-medCV <- function(x, na.rm = FALSE){
+medCV <- function(x, na.rm = FALSE, constant = 1/qnorm(0.75)){
   stopifnot(is.numeric(x))
+  if(na.rm) x <- x[!is.na(x)]
   if(any(x <= 0))
     stop("Your data must be positive!")
-  mad(x, na.rm = na.rm)/median(x, na.rm = na.rm)
+  mad(x, constant = constant)/median(x)
 }
 
-iqrCV <- function(x, na.rm = FALSE){
+iqrCV <- function(x, na.rm = FALSE, type = 7, constant = 2*qnorm(0.75)){
   stopifnot(is.numeric(x))
+  if(na.rm) x <- x[!is.na(x)]
   if(any(x <= 0))
     stop("Your data must be positive!")
-  sIQR(x, na.rm = na.rm)/median(x, na.rm = na.rm)
+  sIQR(x, type = type, constant = constant)/median(x)
 }
